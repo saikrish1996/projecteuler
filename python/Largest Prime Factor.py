33ccfeb331ef20e050bf18ec63901605fe1da2a7
saikrish1996 <saikrshn63@gmail.com>
2024-03-19 20:11:08 +0530

    Update Largest Prime Factor.py

--- python/Largest Prime Factor.py
+++ python/Largest Prime Factor.py
@@ -1,7 +1,29 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 19 16:45:24 2024
+import math  
 
-@author: MSPL
-"""
 
+def getFactors(n):
+    factors = []
+    for i in range(1,  int(math.sqrt(n))):
+        if n % i == 0:
+            if n/i == i:
+                factors.append(i)
+            else:
+                factors.append(i)
+                factors.append(int(n/i))
+    return factors
+      
+    
+factors = getFactors(600851475143)
+factors.sort()
+
+def getPrimefactors(factors):
+    primeFactor = []
+    for num in factors:
+        factors = getFactors(num)
+        if len(factors) == 2:
+            primeFactor.append(num)
+    return primeFactor
+
+primeFactors = getPrimefactors(factors)
+
+primeFactors[len(primeFactors) - 1]
